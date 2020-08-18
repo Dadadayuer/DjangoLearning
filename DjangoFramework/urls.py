7fc87ee18d7d11607f507627446c8e802a6f85a8
Dadadayuer <982742309@qq.com>
2020-08-19 00:23:08 +0800

    添加视图

--- DjangoFramework/urls.py
+++ DjangoFramework/urls.py
@@ -17,7 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from DjangoFramework import views
 
+
+app_name  ='DjangoFramework'
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index)
+    path('',views.index,name="index"),
+    path('<int:question_id>',views.details,name='detail'),
+    path('<int:question_id>/result',views.results,name='results'),
+    path('<int:question_id>/vote',views.vote,name='vote'),
 ]
--- DjangoFramework/views.py
+++ DjangoFramework/views.py
@@ -1,8 +1,34 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from DjangoFramework.models import Question
+from django.shortcuts import render
 
 def index(request):
     import sqlite3
-    conn =sqlite3.connect('DjangoFramework.db')
-    conn.close()
-    return HttpResponse('Hello world! You are at the polls index.')
+    # conn =sqlite3.connect('DjangoFramework.db')
+    # conn.close()
+    # return HttpResponse('Hello world! You are at the polls index.')
+    latest_question_list= Question.objects.order_by('-pub_date')[:5]
+    template =loader.get_template('DjangoFramework/index.html')
+    context={
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context,request))
+
+def details(request,question_id):
+    # return HttpResponse('You are looking at question %s.' %question_id)
+    context={
+        'id':question_id,
+    }
+    return render(request,'DjangoFramework/detail.html',context=context)
+
+def results(request, question_id):
+    response ='You are looking at the result of question %s'
+    return HttpResponse(response%question_id)
+
+
+def vote(request,question_id):
+    return HttpResponse('You are voting on question %s'%question_id)
+
+
+

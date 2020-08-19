c3d8b9f6f44a7bb08588602a71e5be75c22b1b64
Dadadayuer <982742309@qq.com>
2020-08-20 00:37:12 +0800

    视图学习

--- DjangoFramework/urls.py
+++ DjangoFramework/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('<int:question_id>',views.details,name='detail'),
     path('<int:question_id>/result',views.results,name='results'),
     path('<int:question_id>/vote',views.vote,name='vote'),
+    path('<int:questio_id>/unvote',views.unvote,name='unvote')
 ]
--- DjangoFramework/views.py
+++ DjangoFramework/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
-from DjangoFramework.models import Question
-from django.shortcuts import render
+from DjangoFramework.models import Question,Choice
+from django.shortcuts import render,get_object_or_404
+from django.urls import reverse
+
 
 def index(request):
     import sqlite3
@@ -17,18 +19,42 @@ def index(request):
 
 def details(request,question_id):
     # return HttpResponse('You are looking at question %s.' %question_id)
-    context={
-        'id':question_id,
-    }
-    return render(request,'DjangoFramework/detail.html',context=context)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'DjangoFramework/detail.html', {'question': question})
 
 def results(request, question_id):
-    response ='You are looking at the result of question %s'
-    return HttpResponse(response%question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'DjangoFramework/results.html', {'question': question})
 
 
 def vote(request,question_id):
-    return HttpResponse('You are voting on question %s'%question_id)
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        value =request.POST['choice']
+        selected_choice =question.choice_set.get(pk= value)
+    except(KeyError,Choice.DoesNotExist):
+        return render(request,'DjangoFramework/detail.html',{
+            'question':question,
+            'error_message':"You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        url =reverse('results',args=(question.id,))
+        return HttpResponseRedirect(url)
 
+def unvote(request,questio_id):
+    question = get_object_or_404(Question,pk=questio_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except Exception as ex:
+        return render(request,'DjangoFramework/detail.html',{
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes -= 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('results',args=(questio_id,)))
 
 
